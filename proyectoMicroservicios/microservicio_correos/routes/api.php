f3e16cac82998c8259ab61bab93f5dddb6d9b4e9
sofialeyton111 <sleyton@unal.edu.co>
2025-11-27 02:45:49 -0500

    Eliminando archivos antiguos sin el readme

--- proyectoMicroservicios/microservicio_correos/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MailController;
-
-Route::post('/notificaciones', [MailController::class, 'recibir']);
